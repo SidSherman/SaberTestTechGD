f76ea4809fa34e2de4358f9374ab0591cfdc9a71
sidsherman <redneckchan@yandex.ru>
2026-03-25 10:44:10 +1200

    Added new animation and wave attack

--- Source/AleksandrLaskusTestT.Target.cs
+++ Source/AleksandrLaskusTestT.Target.cs
@@ -8,8 +8,9 @@ public class AleksandrLaskusTestTTarget : TargetRules
 	public AleksandrLaskusTestTTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		CppStandard = CppStandardVersion.Cpp20;
 		ExtraModuleNames.Add("AleksandrLaskusTestT");
 	}
 }
--- Source/AleksandrLaskusTestTEditor.Target.cs
+++ Source/AleksandrLaskusTestTEditor.Target.cs
@@ -8,8 +8,8 @@ public class AleksandrLaskusTestTEditorTarget : TargetRules
 	public AleksandrLaskusTestTEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("AleksandrLaskusTestT");
 	}
 }
